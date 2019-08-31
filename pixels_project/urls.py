7cc4b47687c782bbec89991ee7531a3fddbb914f
Sunday Brian <brians931@gmail.com>
2019-08-31 16:46:05 +0300

    fixed the template reference for the logout in the projectsurl.py

--- pixels_project/urls.py
+++ pixels_project/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('register/',user_views.register,name='register'),
     path('',include('insta.urls')),
     path('login/',auth_views.LoginView.as_view(template_name='user/login.html'),name='login'),
-    path('login/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='user/logout.html'),name='logout'),
 
 ]
