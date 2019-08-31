249019e72138957d9a22c7371bacc4c704e08f80
Sunday Brian <brians931@gmail.com>
2019-08-31 16:18:52 +0300

    setting up the django auth views for the login and logout

--- pixels_project/urls.py
+++ pixels_project/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contri.auth import views as auth_views
 from django.urls import path,include
 from users import views as user_views
 
@@ -22,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',user_views.register,name='register'),
     path('',include('insta.urls')),
-
+    path('login/',auth_views.LoginView.as_view(tempalate_name='user/login.html'),name='login'),
+    path('login/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
 
 ]
