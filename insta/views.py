4057b4aed023705246670545346b65a91d61ab7f
Sunday Brian <brians931@gmail.com>
2019-08-31 12:12:01 +0300

    add dummy post to use on the home page

--- insta/views.py
+++ insta/views.py
@@ -3,6 +3,29 @@ from django.http import HttpResponse,Http404
 
 # Create your views here.
 
+posts=[
+    {
+        'username':'Brians Brians',
+        'title':'Out and About',
+        'poster_path':'static/img/ironman1.jpg',
+        'date_posted':'August 31,2019'
+    },
+      {
+        'username':'Brians Omwami',
+        'title':'iron man',
+        'poster_path':'static/img/ironman1.jpg',
+        'date_posted':'August 29,2019'
+    }
+    ,
+      {
+        'username':'Sunday Omwami',
+        'title':'iron man',
+        'poster_path':'static/img/ironman1.jpg',
+        'date_posted':'August 30,2019'
+    }
+
+]
+
 def home(request):
-    return render(request,'insta/home.html')
+    return render(request,'insta/home.html',{'posts':posts})
 
