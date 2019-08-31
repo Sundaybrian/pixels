7e8172873c05a75ada847fed6e6a7fb013534702
Sunday Brian <brians931@gmail.com>
2019-08-31 16:19:25 +0300

    fixed the typo for the path imports in settings.py

--- pixels_project/urls.py
+++ pixels_project/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',user_views.register,name='register'),
     path('',include('insta.urls')),
-    path('login/',auth_views.LoginView.as_view(tempalate_name='user/login.html'),name='login'),
+    path('login/',auth_views.LoginView.as_view(template_name='user/login.html'),name='login'),
     path('login/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
 
 ]
