bba036ef50b6bcad5436b58a1045a1842f66e1ec
Sunday Brian <brians931@gmail.com>
2019-09-01 00:51:27 +0300

    register the like model to the admin

--- insta/admin.py
+++ insta/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Comment,Image,tags
+from .models import Comment,Image,tags,Like
+
 
 # Register your models here.
 
 admin.site.register(Comment)
 admin.site.register(Image)
 admin.site.register(tags)
+admin.site.register(Like)
