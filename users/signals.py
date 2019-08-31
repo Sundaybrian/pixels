09f5067581d4d7c586c2af83f669375d0846775d
Sunday Brian <brians931@gmail.com>
2019-08-31 21:12:09 +0300

    add the save profile function to save the user profile once a user is saved

--- users/signals.py
+++ users/signals.py
@@ -29,8 +29,18 @@ def create_profile(sender,instance,created,**kwargs):
     if created:
         Profile.objects.create(user=instance)
 
+'''
+    save profile once a user is saved
+'''
+@receiver(post_save,sender=User)
+def save_profile(sender,instance,created,**kwargs):
+    instance.profile.save()
+  
 
-        
+
+
+
+
 
 
 
