f368cc5da37e6756f6699df5e4864a4e9c01320b
Sunday Brian <brians931@gmail.com>
2019-08-31 23:49:14 +0300

    overwrite the save method in the profile model to resize the profile photos

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     # one user can have one profile and one profile is associated with one user
@@ -10,6 +11,22 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username}-Profile'
+
+    def save(self):
+        '''
+        overwrite save method to resize images
+        '''
+        super().save()
+
+        img=Image.open(self.profile_photo.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size=(300,300)
+            img.thumbnail(output_size)
+            img.save(self.profile_photo.path)
+
+
+
 
 
 
