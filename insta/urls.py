b8f3f8ec84b812b2fdd7e4224e5b78814c3bb434
Sunday Brian <brians931@gmail.com>
2019-09-01 17:15:48 +0300

    created the class view url to route to the update form template page

--- insta/urls.py
+++ insta/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 from django.conf.urls import url
+from .views import PostUpdateView
 
 urlpatterns=[
     path('',views.home,name='insta-home'),
     path('post/new/',views.newInstaPost,name='new-insta-post'),
     path('post/<int:pk>/',views.postDetail,name='post-detail'),
+    path('post/<int:pk>/update',views.PostUpdateView.as_view(),name='post-update'),
     
 ]
