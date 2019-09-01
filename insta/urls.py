3cbdd088e9b357ecad2e9cb5b50547fdff5a1a41
Sunday Brian <brians931@gmail.com>
2019-09-01 15:02:04 +0300

    add a test url for the postdetails

--- insta/urls.py
+++ insta/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls import url
 urlpatterns=[
     path('',views.home,name='insta-home'),
     path('post/new/',views.newInstaPost,name='new-insta-post'),
+    path('post/<int:pk>/',views.postDetail,name='post-detail'),
     
 ]
