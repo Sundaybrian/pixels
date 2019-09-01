aa9d9c14f07b6ae72c77bcbe2a6425d625b401f5
Sunday Brian <brians931@gmail.com>
2019-09-01 18:07:32 +0300

    add the postdelete to the urls

--- insta/urls.py
+++ insta/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
 from django.conf.urls import url
-from .views import PostUpdateView
+from .views import PostUpdateView,PostDeleteView
 
 urlpatterns=[
     path('',views.home,name='insta-home'),
     path('post/new/',views.newInstaPost,name='new-insta-post'),
     path('post/<int:pk>/',views.postDetail,name='post-detail'),
     path('post/<int:pk>/update',views.PostUpdateView.as_view(),name='post-update'),
+    path('post/<int:pk>/delete',views.PostDeleteView.as_view(),name='post-delete'),
     
 ]
