ccc784e1592d40254cea22a5810b98fe8c672e3f
Sunday Brian <brians931@gmail.com>
2019-09-01 12:44:58 +0300

    register the tinymce url in the projects settings

--- pixels_project/urls.py
+++ pixels_project/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path,include
+from django.conf.urls import url
 from users import views as user_views
 
 from django.conf import settings
@@ -29,6 +30,7 @@ urlpatterns = [
     path('',include('insta.urls')),
     path('login/',auth_views.LoginView.as_view(template_name='user/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='user/logout.html'),name='logout'),
+    url(r'^tinymce/',include('tinymce.urls')),
 
 ]
 
