1b6b953e0e7279c424712da87f4c2e15e1d17699
Sunday Brian <brians931@gmail.com>
2019-09-01 10:41:32 +0300

    remove the dummy post data and replace withe post data from the db

--- insta/views.py
+++ insta/views.py
@@ -1,31 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse,Http404
+from .models import Image,Comment,Like
 
 # Create your views here.
 
-posts=[
-    {
-        'username':'Brians Brians',
-        'title':'Out and About',
-        'poster_path':'static/img/ironman1.jpg',
-        'date_posted':'August 31,2019'
-    },
-      {
-        'username':'Brians Omwami',
-        'title':'iron man',
-        'poster_path':'static/img/ironman1.jpg',
-        'date_posted':'August 29,2019'
-    }
-    ,
-      {
-        'username':'Sunday Omwami',
-        'title':'iron man',
-        'poster_path':'static/img/ironman1.jpg',
-        'date_posted':'August 30,2019'
-    }
-
-]
-
 def home(request):
+  
+    posts=Image.get_imgs()
+    
     return render(request,'insta/home.html',{'posts':posts})
 
